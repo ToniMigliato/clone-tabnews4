6f1be41c7a0aa17ef1efaee1ae92e121afc2bb40
ToniMigliato <tonimigliato@gmail.com>
2024-05-22 09:19:42 -0300

    finish endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Alunos são acima da média." });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionQuery = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionQuery.rows[0].server_version;
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsQuery.rows[0].max_connections;
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsQuery = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = openedConnectionsQuery.rows[0].count;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 
 export default status;
