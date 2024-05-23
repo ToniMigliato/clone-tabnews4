64657ac386250baab7c443704d3d7193dbee4976
ToniMigliato <tonimigliato@gmail.com>
2024-05-23 08:34:46 -0300

    create `/migration` endpoint connection with db

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,8 +3,9 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
   const defaultMigrationOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
